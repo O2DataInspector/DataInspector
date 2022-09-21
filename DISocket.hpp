22e2e825815b1a9d31c9d1b8346db950380df993
Arkadiusz Wolk <awolk@student.agh.edu.pl>
2022-09-21 19:16:41 +0200

    DI | DISocket - different payload types

--- DISocket.hpp
+++ DISocket.hpp
@@ -10,6 +10,19 @@
 #include "boost/endian/conversion.hpp"
 #include <sstream>
 
+template <typename... T>
+struct always_static_assert : std::false_type {
+};
+
+template <typename... T>
+inline constexpr bool always_static_assert_v = always_static_assert<T...>::value;
+
+template <typename T>
+inline constexpr bool has_serialize_v = std::is_same_v<char* (T::*)(uint64_t&), typeof &T::serialize>;
+
+template <typename T>
+inline constexpr bool has_deserialize_v = std::is_same_v<T (T::*)(char*), typeof &T::deserialize>;
+
 struct DIMessage {
   struct Header {
     enum class Type : uint32_t {
@@ -20,17 +33,59 @@ struct DIMessage {
       INSPECT_OFF = 5
     };
 
+    Header() = default;
     Header(Type type, uint64_t payloadSize) : type(type), payloadSize(payloadSize) {}
 
     Type type;
     uint64_t payloadSize;
   };
 
-  DIMessage(Header::Type type, const std::string& payload) : header(type, payload.size()), payload(payload) {}
+  template<typename T>
+  DIMessage(Header::Type type, T&& payload) : header()
+  {
+    header.type = type;
+
+    if constexpr (std::is_base_of_v<std::string, T>) {
+      header.payloadSize = payload.size();
+      this->payload = new char[header.payloadSize];
+      std::memcpy(this->payload, payload.data(), header.payloadSize);
+    } else if constexpr (std::is_integral_v<T>) {
+      header.payloadSize = sizeof(T);
+      payload = boost::endian::native_to_little(payload);
+      this->payload = new char[header.payloadSize];
+      std::memcpy(this->payload, &payload, header.payloadSize);
+    } else if constexpr (has_serialize_v<T>) {
+      this->payload = payload.serialize(header.payloadSize);
+    } else {
+      static_assert(always_static_assert_v<T>, "DISocket: Cannot create message of this type.");
+    }
+  }
+  DIMessage(Header::Type type, const char* data, uint64_t size) : header(type, size) {
+    payload = new char[size];
+    std::memcpy(payload, data, size);
+  }
   DIMessage(Header::Type type) : header(type, 0), payload() {}
 
+  ~DIMessage()
+  {
+    delete[] payload;
+  }
+
+  template<typename T>
+  decltype(auto) get() {
+    if constexpr (std::is_same_v<std::string, T>) {
+      return std::string{payload, header.payloadSize};
+    } else if constexpr (std::is_integral_v<T>) {
+      return boost::endian::little_to_native(*((T*) payload));
+    } else if constexpr (has_deserialize_v<T>) {
+      return T::deserialize(payload);
+    } else {
+      static_assert(always_static_assert_v<T>, "DISocket: Cannot create object of this type.");
+    }
+  }
+
   Header header;
-  std::string payload;
+  char* payload;
 };
 
 static boost::asio::io_context ioContext;
@@ -38,7 +93,7 @@ static boost::asio::io_context ioContext;
 #define ASIO_CATCH(customMessage) catch(boost::system::system_error& err){ auto msg = std::string{err.what()}; auto code = std::to_string(err.code().value()); throw std::runtime_error{"Exception in DataInspector (boost_code=" + code + ", boost_msg=" + msg + ") - " + customMessage}; }
 
 class DISocket {
-public:
+ public:
   DISocket(std::unique_ptr<boost::asio::ip::tcp::socket> socket) : socket(std::move(socket)) {}
   DISocket(DISocket&& diSocket)  noexcept : socket(std::move(diSocket.socket)) {}
 
@@ -57,9 +112,9 @@ public:
   {
     try {
       char header[12];
-      uint32_t type_LE = boost::endian::native_to_little(static_cast<uint32_t>(message.header.type));
+      uint32_t messageType_LE = boost::endian::native_to_little(static_cast<uint32_t>(message.header.type));
       uint64_t payloadSize_LE = boost::endian::native_to_little(message.header.payloadSize);
-      memcpy(header, &type_LE, 4);
+      memcpy(header, &messageType_LE, 4);
       memcpy(header + 4, &payloadSize_LE, 8);
 
       // send header
@@ -83,7 +138,7 @@ public:
       char header[12];
       socket->read_some(boost::asio::buffer(header, 12));
 
-      uint32_t type = boost::endian::little_to_native(*((uint32_t*)header));
+      uint32_t messageType = boost::endian::little_to_native(*((uint32_t*)header));
       uint64_t payloadSize = boost::endian::little_to_native(*((uint64_t*)(header + 4)));
 
       // read payload
@@ -94,9 +149,9 @@ public:
           read += socket->read_some(boost::asio::buffer(payload + read, payloadSize - read));
         }
 
-        return DIMessage{static_cast<DIMessage::Header::Type>(type), std::string{payload, payloadSize}};
+        return DIMessage{static_cast<DIMessage::Header::Type>(messageType), payload, payloadSize};
       } else {
-        return DIMessage{static_cast<DIMessage::Header::Type>(type)};
+        return DIMessage{static_cast<DIMessage::Header::Type>(messageType)};
       }
     }
     ASIO_CATCH("DISocket::receive")
@@ -115,7 +170,7 @@ public:
     socket->close();
   }
 
-private:
+ private:
   std::unique_ptr<boost::asio::ip::tcp::socket> socket;
 };
 
