f78e9ea4a347ffa733202a676f7eee607c0ca26b
Arkadiusz Wolk <awolk@student.agh.edu.pl>
2022-11-18 15:13:38 +0100

    DI | update build & execute scripts

--- domain/AnalysesService.cpp
+++ domain/AnalysesService.cpp
@@ -19,7 +19,7 @@ Analysis AnalysesService::get(const std::string& analysisId) {
 std::vector<std::string> AnalysesService::listWorkflows(const std::string& analysisId) {
   std::vector<std::string> workflows;
 
-  boost::filesystem::recursive_directory_iterator it(basePath + "latest-build-" + analysisId + "-o2/bin"), end;
+  boost::filesystem::recursive_directory_iterator it(basePath + "latest-analysis-" + analysisId + "-o2/bin"), end;
   while (it != end) {
     workflows.push_back(it->path().filename().string());
     ++it;
