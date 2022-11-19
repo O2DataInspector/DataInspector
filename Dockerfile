f158f7595ea0879c6306b15556cbcfaa3d04ac38
Arkadiusz Wolk <awolk@student.agh.edu.pl>
2022-11-19 21:08:45 +0100

    DI | change script & fix

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,9 @@ FROM arczipt/di-proxy-builder
 WORKDIR /app
 COPY . /app/proxy
 
+RUN echo "export ALIBUILD_WORK_DIR=/di/alice/sw" >> .bashrc
+RUN echo "eval \"`alienv shell-helper`\"" >> .bashrc
+
 WORKDIR /app/proxy/build
 RUN cmake ..
 RUN cmake --build .
--- infrastructure/MongoAnalysisRepository.cpp
+++ infrastructure/MongoAnalysisRepository.cpp
@@ -1,5 +1,6 @@
 #include "infrastructure/MongoAnalysisRepository.h"
 #include <stdexcept>
+#include <iostream>
 
 std::string MongoAnalysisRepository::save(const Analysis& analysis) {
   bson_t *doc;
@@ -48,11 +49,11 @@ std::string MongoAnalysisRepository::save(const Analysis& analysis) {
 }
 
 std::vector<Analysis> MongoAnalysisRepository::getAnalyses(int page, int count) {
-  const bson_oid_t* oid;
   bson_iter_t iter;
   bson_t *query;
   bson_iter_t child;
   const bson_t *doc;
+  const bson_oid_t *oid_ptr;
   mongoc_cursor_t *cursor;
   mongoc_client_t *client;
   mongoc_collection_t *collection;
@@ -61,8 +62,17 @@ std::vector<Analysis> MongoAnalysisRepository::getAnalyses(int page, int count) 
   collection = mongoc_client_get_collection(client, "diProxy", "analyses");
 
   query = bson_new();
-  cursor = mongoc_collection_find_with_opts(
-          collection, query, NULL, NULL);
+  auto* matchHeader = bson_new();
+  auto* match = bson_new();
+  auto* limit = bson_new();
+  auto* skip = bson_new();
+  BSON_APPEND_DOCUMENT(matchHeader, "$match", match);
+  BSON_APPEND_DOCUMENT(query, "0", matchHeader);
+  BSON_APPEND_INT64(skip, "$skip", page*count);
+  BSON_APPEND_DOCUMENT(query, "1", skip);
+  BSON_APPEND_INT64(limit, "$limit", count);
+  BSON_APPEND_DOCUMENT(query, "2", limit);
+  cursor = mongoc_collection_aggregate(collection, MONGOC_QUERY_NONE, query, NULL, NULL);
 
   std::vector<Analysis> analyses;
   char str_oid[25];
@@ -70,8 +80,8 @@ std::vector<Analysis> MongoAnalysisRepository::getAnalyses(int page, int count) 
   {
     Analysis analysis;
     bson_iter_init_find(&iter, doc, "_id");
-    oid = bson_iter_oid(&iter);
-    bson_oid_to_string(oid, str_oid);
+    oid_ptr = bson_iter_oid(&iter);
+    bson_oid_to_string(oid_ptr, str_oid);
     analysis.id = str_oid;
 
     bson_iter_init_find(&iter, doc, "buildStatus");
@@ -95,7 +105,13 @@ std::vector<Analysis> MongoAnalysisRepository::getAnalyses(int page, int count) 
 
     bson_iter_init_find(&iter, doc, "branch");
     analysis.branch = bson_iter_utf8(&iter, NULL);
+
+    analyses.push_back(analysis);
   }
+  bson_destroy(match);
+  bson_destroy(matchHeader);
+  bson_destroy(skip);
+  bson_destroy(limit);
   bson_destroy(query);
   mongoc_cursor_destroy(cursor);
   mongoc_collection_destroy(collection);
@@ -159,72 +175,73 @@ Analysis MongoAnalysisRepository::get(const std::string& analysisId) {
 }
 
 void MongoAnalysisRepository::appendLogs(const std::string& analysisId, const std::vector<std::string>& logs) {
-  bson_oid_t oid;
-  bson_iter_t iter;
-  bson_iter_t child_iter;
-  bson_error_t error;
-  bson_t *query;
-  bson_t child;
-  const bson_t *doc;
-  bson_t *docc;
-  mongoc_cursor_t *cursor;
-  mongoc_client_t *client;
-  mongoc_collection_t *collection;
-  std::vector<std::string> logs_copy = logs;
-  std::string tmp;
-  int i = 0;
-  char key[200];
-
-  client = mongoc_client_pool_pop (pool);
-  collection = mongoc_client_get_collection(client, "diProxy", "analyses");
-
-  bson_oid_init_from_string(&oid, analysisId.c_str());
-  query = BCON_NEW("_id", BCON_OID(&oid));
-
-  Analysis analysis = MongoAnalysisRepository::get(analysisId);
-
-  cursor = mongoc_collection_find_with_opts(
-	collection, query, NULL, NULL);
-  
-  if(mongoc_cursor_next(cursor, &doc)) 
-  {
-  	bson_iter_init_find (&iter, doc, "logs");
-  	bson_iter_recurse (&iter, &child_iter);
-  	while (bson_iter_next (&child_iter)) {
-      		tmp = bson_iter_utf8(&child_iter, NULL);
-      		logs_copy.emplace_back(tmp);
-  	}
-  	docc = bson_new();
-  	BSON_APPEND_OID(docc, "_id", &oid);
-
-  	BSON_APPEND_UTF8(docc, "url", analysis.url.c_str());
-  	BSON_APPEND_UTF8(docc, "name", analysis.name.c_str());
-    BSON_APPEND_UTF8(docc, "branch", analysis.branch.c_str());
-  	BSON_APPEND_INT32(docc, "buildStatus", (int) analysis.buildStatus);
-  	BSON_APPEND_ARRAY_BEGIN(docc, "logs", &child);
-  	while(logs_copy.size() != 0)
-  	{
-    		snprintf(key, sizeof key, "%d", i);
-    		BSON_APPEND_UTF8(&child, key, logs_copy.back().c_str());
-    		logs_copy.pop_back();
-    		i++;
-  	}
-  	bson_append_array_end(docc, &child);
-  	if(!mongoc_collection_insert_one(
-     	    collection, docc, NULL, NULL, &error))
-	/* fixme: update instead of insert */
-  	{
-		fprintf(stderr, "%s\n", error.message);
-  	}
-        bson_destroy(docc);
-  }
-  else
-  {
-	fprintf(stderr, "haven't found proper id\n");
-  }
-  bson_destroy(query);
-  mongoc_cursor_destroy(cursor);
-  mongoc_client_pool_push (pool, client);
+  std::cout << "MongoAnalysisRepository::appendLogs - NOT IMPLEMENTED" << std::endl;
+//  bson_oid_t oid;
+//  bson_iter_t iter;
+//  bson_iter_t child_iter;
+//  bson_error_t error;
+//  bson_t *query;
+//  bson_t child;
+//  const bson_t *doc;
+//  bson_t *docc;
+//  mongoc_cursor_t *cursor;
+//  mongoc_client_t *client;
+//  mongoc_collection_t *collection;
+//  std::vector<std::string> logs_copy = logs;
+//  std::string tmp;
+//  int i = 0;
+//  char key[200];
+//
+//  client = mongoc_client_pool_pop (pool);
+//  collection = mongoc_client_get_collection(client, "diProxy", "analyses");
+//
+//  bson_oid_init_from_string(&oid, analysisId.c_str());
+//  query = BCON_NEW("_id", BCON_OID(&oid));
+//
+//  Analysis analysis = MongoAnalysisRepository::get(analysisId);
+//
+//  cursor = mongoc_collection_find_with_opts(
+//	collection, query, NULL, NULL);
+//
+//  if(mongoc_cursor_next(cursor, &doc))
+//  {
+//  	bson_iter_init_find (&iter, doc, "logs");
+//  	bson_iter_recurse (&iter, &child_iter);
+//  	while (bson_iter_next (&child_iter)) {
+//      		tmp = bson_iter_utf8(&child_iter, NULL);
+//      		logs_copy.emplace_back(tmp);
+//  	}
+//  	docc = bson_new();
+//  	BSON_APPEND_OID(docc, "_id", &oid);
+//
+//  	BSON_APPEND_UTF8(docc, "url", analysis.url.c_str());
+//  	BSON_APPEND_UTF8(docc, "name", analysis.name.c_str());
+//    BSON_APPEND_UTF8(docc, "branch", analysis.branch.c_str());
+//  	BSON_APPEND_INT32(docc, "buildStatus", (int) analysis.buildStatus);
+//  	BSON_APPEND_ARRAY_BEGIN(docc, "logs", &child);
+//  	while(logs_copy.size() != 0)
+//  	{
+//    		snprintf(key, sizeof key, "%d", i);
+//    		BSON_APPEND_UTF8(&child, key, logs_copy.back().c_str());
+//    		logs_copy.pop_back();
+//    		i++;
+//  	}
+//  	bson_append_array_end(docc, &child);
+//  	if(!mongoc_collection_insert_one(
+//     	    collection, docc, NULL, NULL, &error))
+//	/* fixme: update instead of insert */
+//  	{
+//		fprintf(stderr, "%s\n", error.message);
+//  	}
+//        bson_destroy(docc);
+//  }
+//  else
+//  {
+//	fprintf(stderr, "haven't found proper id\n");
+//  }
+//  bson_destroy(query);
+//  mongoc_cursor_destroy(cursor);
+//  mongoc_client_pool_push (pool, client);
 }
 
 void MongoAnalysisRepository::updateStatus(const std::string& analysisId, Analysis::BuildStatus status) {
